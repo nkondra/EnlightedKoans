a52da787fd2eabc099bf8ffcea5a95a6865ba17f
Nathan Kondra <nathan@cagedpheonix.com>
2012-08-15 16:39:59 -0700

    Repair the test_small_integers_have_fixed_ids

--- about_objects.rb
+++ about_objects.rb
@@ -37,13 +37,14 @@ class AboutObjects < EdgeCase::Koan
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 1, 0.object_id
+    assert_equal 3, 1.object_id
+    assert_equal 5, 2.object_id
+    assert_equal 201, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
+    # It appears to follow the thought of *2 + 1
   end
 
   def test_clone_creates_a_different_object
