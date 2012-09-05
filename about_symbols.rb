eb70f67db01020a26db239ae13991ea4e5cdc52c
Nathan Kondra <nathan@cagedpheonix.com>
2012-09-05 11:28:54 -0700

    Repair the test_symbols_cannot_be_concatenated

--- about_symbols.rb
+++ about_symbols.rb
@@ -87,7 +87,7 @@ class AboutSymbols < EdgeCase::Koan
 
   def test_symbols_cannot_be_concatenated
     # Exceptions will be pondered further down the path
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       :cats + :dogs
     end
   end
