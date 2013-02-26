4a9472a767256adf44e1eacc8ec8dd76e2bfeaf1
Nathan Kondra <nathan@thinkprofits.com>
2013-02-26 14:00:10 -0800

    Repair test_calling_globale_methods_with_wring_number_of_arguments

--- about_methods.rb
+++ about_methods.rb
@@ -33,15 +33,15 @@ class AboutMethods < EdgeCase::Koan
   # NOTE: wrong number of argument is not a SYNTAX error, but a
   # runtime error.
   def test_calling_global_methods_with_wrong_number_of_arguments
-    exception = assert_raise(___) do
+    exception = assert_raise(ArgumentError) do
       my_global_method
     end
-    assert_match(/__/, exception.message)
+    assert_match(/0 for 2/, exception.message)
 
-    exception = assert_raise(___) do
+    exception = assert_raise(ArgumentError) do
       my_global_method(1,2,3)
     end
-    assert_match(/__/, exception.message)
+    assert_match(/3 for 2/, exception.message)
   end
 
   # ------------------------------------------------------------------
