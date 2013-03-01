63c85a5f745ed7f4a6a0271b183996c01c847c6f
Nathan Kondra <nathan@thinkprofits.com>
2013-02-28 17:59:05 -0800

    Repair test_who_wins_with_explicit_scoping_on_class_definition

--- about_constants.rb
+++ about_constants.rb
@@ -79,10 +79,14 @@ class AboutConstants < EdgeCase::Koan
   end
 
   def test_who_wins_with_explicit_scoping_on_class_definition
-    assert_equal __, MyAnimals::Oyster.new.legs_in_oyster
+    assert_equal 4, MyAnimals::Oyster.new.legs_in_oyster
   end
 
   # QUESTION: Now which has precedence: The constant in the lexical
   # scope, or the constant from the inheritance hierarchy?  Why is it
   # different than the previous answer?
+  #
+  # As it has been explained to me, because we are setting the class directly we never actually run into
+  # the MyAnimal scope we jump right over the reference with the :: notation hence our chaining 
+  # never hits the original constant in the myanimal class
 end
