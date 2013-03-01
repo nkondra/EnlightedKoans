7d522842a2365888740009a5e3e41ef13de5fcaa
Nathan Kondra <nathan@thinkprofits.com>
2013-03-01 11:09:07 -0800

    Repair test_equalateral_triangles_have_equal_sides

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  triangle_sides = [a, b, c]
+  equal_sides = triangle_sides.uniq.count
+  if equal_sides == 3
+  	return :scalene
+  elsif equal_sides == 2
+  	return :isosceles
+  else
+  	return :equilateral
+  end
 end
 
-# Error class used in part 2.  No need to change this code.
+# Error class used in part 2.  No need to cha nge this code.
 class TriangleError < StandardError
 end
