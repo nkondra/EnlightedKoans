72e4601c81476632b2e435a3dfb1ede3c34c92c9
Nathan Kondra <nathan@thinkprofits.com>
2013-03-08 17:08:10 -0800

    Repair test_illegal_triangles_throw_exceptions

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  triangle_sides = [a, b, c]
+  triangle_sides = [a, b, c].sort
+
+  raise TriangleError unless triangle_sides[0] > 0
+  raise TriangleError unless triangle_sides[0] + triangle_sides[1] > triangle_sides[2]
+
   equal_sides = triangle_sides.uniq.count
   if equal_sides == 3
   	return :scalene
@@ -24,6 +28,14 @@ def triangle(a, b, c)
   	return :equilateral
   end
 end
+
+# Code solution i found online to refactor my code even further *Impressive*
+#
+# def triangle(a, b, c)
+#   a, b, c = sides = [a, b, c].sort
+#   raise TriangleError unless a > 0 and a + b > c
+#   {1 => :equilateral, 2 => :isosceles, 3 => :scalene}[sides.uniq.size]
+# end
 
 # Error class used in part 2.  No need to cha nge this code.
 class TriangleError < StandardError
