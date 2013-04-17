b6d76c4e43d88b066f58292a4322934373e8e904
Nathan Kondra <nathan@thinkprofits.com>
2013-04-17 16:26:09 -0700

    test_different_objects_have_different_instance_variables

--- about_classes.rb
+++ about_classes.rb
@@ -133,7 +133,7 @@ class AboutClasses < EdgeCase::Koan
     fido = Dog6.new("Fido")
     rover = Dog6.new("Rover")
 
-    assert_equal __, rover.name != fido.name
+    assert_equal true, rover.name != fido.name
   end
 
   # ------------------------------------------------------------------
