d29f6fe8f4d2823399fcf41a02cee8411306fb32
Nathan Kondra <nathan@thinkprofits.com>
2013-04-17 19:59:43 -0700

    test_you_can_define_methods_on_individual_objects

--- about_class_methods.rb
+++ about_class_methods.rb
@@ -31,7 +31,7 @@ class AboutClassMethods < EdgeCase::Koan
     def fido.wag
       :fidos_wag
     end
-    assert_equal __, fido.wag
+    assert_equal :fidos_wag, fido.wag
   end
 
   def test_other_objects_are_not_affected_by_these_singleton_methods
