6dbc203f01b8d089145bd7a2bbf37798ac4586a0
Nathan Kondra <nathan@thinkprofits.com>
2013-04-17 17:29:37 -0700

    test_even_existing_built_in_classes_can_be_reopened

--- about_open_classes.rb
+++ about_open_classes.rb
@@ -36,8 +36,8 @@ class AboutOpenClasses < EdgeCase::Koan
   end
 
   def test_even_existing_built_in_classes_can_be_reopened
-    assert_equal __, 1.even?
-    assert_equal __, 2.even?
+    assert_equal false, 1.even?
+    assert_equal true, 2.even?
   end
 
   # NOTE: To understand why we need the :: before Integer, you need to
