6d269679735e59f68ba1c858a41f3d581e76cf9a
Nathan Kondra <nathan@thinkprofits.com>
2013-04-21 11:22:47 -0700

    test_catching_messages_makes_respond_to_lie

--- about_message_passing.rb
+++ about_message_passing.rb
@@ -132,7 +132,7 @@ class AboutMessagePassing < EdgeCase::Koan
     assert_nothing_raised(NoMethodError) do
       catcher.any_method
     end
-    assert_equal __, catcher.respond_to?(:any_method)
+    assert_equal false, catcher.respond_to?(:any_method)
   end
 
   # ------------------------------------------------------------------
